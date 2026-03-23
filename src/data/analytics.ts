0c6ed980166d599e244cfda1349cab57b4ad5325
JunyaoHu <hujunyao0329@gmail.com>
2026-03-23 16:53:27 +0800

    feat: integrate Google Analytics and 51LA scripts for enhanced tracking capabilities

---
+++ src/data/analytics.ts
@@ -0,0 +1,14 @@
+export const LA_SCRIPT_SRC = '//sdk.51.la/js-sdk-pro.min.js';
+
+export const LA_CONFIG = {
+	enabled: true,
+	id: '3PMwBK5KTJngd23m',
+	ck: '3PMwBK5KTJngd23m',
+	autoTrack: true,
+	screenRecord: true,
+} as const;
+
+export const GA_CONFIG = {
+	enabled: true,
+	measurementId: 'G-2CXNDVRGGM',
+} as const;
